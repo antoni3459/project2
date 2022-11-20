b171e5242133077174e8905fc04623e6ed0e7968
antoni3459 <114168233+antoni3459@users.noreply.github.com>
2022-11-20 21:26:13 +0100

    supre

--- pas utile 2/O3DLibrary.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define O3DLIBRARY_API __declspec(dllexport)
-
--- pas utile/O3Dlibrairy/O3Dlibrairy.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define O3DLIBRAIRY_API __declspec(dllexport)
-
-extern "C" O3DLIBRAIRY_API void Test();
