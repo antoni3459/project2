6661e65e89b006c965312f5ce6d071f957abd093
antoni3459 <114168233+antoni3459@users.noreply.github.com>
2022-11-26 15:22:56 +0100

    dv

--- projet Libre !/quiz/quiz/Réponse.cpp
+++ projet Libre !/quiz/quiz/Réponse.cpp
@@ -1,7 +1,8 @@
 #include "Réponse.h"
+#include "Question.h"
 
 bool Réponse::GetReponse()
 {
     std::string _question = "";
-    _question.GetQuestion();
+    x->GetX();
 }
--- projet Libre !/quiz/quiz/Réponse.h
+++ projet Libre !/quiz/quiz/Réponse.h
@@ -7,7 +7,7 @@ class Réponse
 {
 private:
 	std::string reponse[2] = { "1939-1945","1914-1918" };
-	
+	int* x = nullptr;
 public:
 	Réponse() = default;
 	~Réponse() = default;
